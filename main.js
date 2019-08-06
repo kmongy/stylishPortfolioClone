40bd8bd22f78df91567f128afd6463aba3590f3d
Kong Yang <kmongy05@gmail.com>
2019-08-06 13:34:09 -0500

    Added a scroll to top button with js.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const navigation = document.querySelector('.navigation-sidebar');
 const navigationBtn = document.querySelectorAll('.open-close-toggle');
 
+const buttonUp = document.querySelector('.button-up');
+
 navigationBtn.forEach(function(button) {
   button.addEventListener('click', toggleMenu);
 });
@@ -8,3 +10,21 @@ navigationBtn.forEach(function(button) {
 function toggleMenu() {
   navigation.classList.toggle('nav-visible');
 }
+
+function scrollToTop(duration) {
+  if(duration <= 0) {
+    return;
+  }
+  const difference = 0 - document.documentElement.scrollTop;
+  const perTick = difference / duration * 10;
+
+  setTimeout(() => {
+    document.documentElement.scrollTop = document.documentElement.scrollTop + perTick;
+    if (document.documentElement.scrollTip === 0) {
+      return;
+    }
+    scrollToTop(duration - 10);
+  }, 10);
+}
+
+buttonUp.addEventListener('click', () => scrollToTop(400));
